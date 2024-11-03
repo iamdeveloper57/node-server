f12ff851752aa5d1554f527be8bfca8461a5cd18
developer <agraharirehan57@gmail.com>
2024-11-03 15:10:57 +0545

    db on line

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/personData';
+// const url = 'mongodb://localhost:27017/personData';
+// const url = "mongodb+srv://dbuser:iamdbuser@cluster0.gxig0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const url = "mongodb+srv://agraharirehan57:thisismongodb@cluster0.45qqn.mongodb.net/"
 
 mongoose.connect(url, {
     
